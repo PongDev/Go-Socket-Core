4d85897a620e19f525c442cb66e2940e45a82ea9
miello <partearth1212@gmail.com>
2023-04-18 15:06:46 +0700

    feat: draft for channel module

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,18 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/PongDev/Go-Socket-Core/services"
+	"github.com/PongDev/Go-Socket-Core/utils"
+	"github.com/gin-gonic/gin"
+)
 
 func SetupRouter(r *gin.Engine) {
+	hub := utils.NewHub()
+
+	channelService := services.NewChannelService(&hub)
+
+	r.POST("/channel", channelService.CreateChannel)
+	r.POST("/channel/:channelId", channelService.HandleMessage)
+	r.GET("/channel/:channelId", channelService.ConnectChannel)
+	r.DELETE("/channel/:channelId", channelService.DeleteChannel)
 }
